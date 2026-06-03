257777661c4e39505c3860d6f8eedee69f3c1544
Ganesh Rajak <ganeshrajak455@gmail.com>
2026-06-03 17:33:41 +0545

    home page search buttom color

--- lib/Home_page.dart
+++ lib/Home_page.dart
@@ -5,10 +5,54 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: Text("WhatsApp"), backgroundColor: Colors.blueGrey,
-    actions: [IconButton(onPressed: (){}, icon: Icon(Icons.camera_outdoor)),
-              IconButton(onPressed: (){}, icon: Icon(Icons.three_k_plus))],     
-    ),
+    return Scaffold(
+      drawer: Drawer(),
+        backgroundColor: const Color(0xFF111B21),
+      appBar: AppBar(
+        title: Text("WhatsApp", style: TextStyle(fontSize: 20)),
+        actions: [
+          IconButton(onPressed: () {}, icon: Icon(Icons.camera_alt_sharp)),
+          IconButton(onPressed: () {}, icon: Icon(Icons.more_vert)),
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.all(20),
+          child: Column(
+            children: [
+              Card(color: Color(0xFF111B21),
+                child: Padding(padding: EdgeInsets.all(20),
+                child: TextField(
+                  decoration: InputDecoration(
+                    hintText: "Search",
+                    prefixIcon: Icon(Icons.search),
+                    border: OutlineInputBorder(borderRadius: BorderRadius.circular(30)),
+                  ),),
+                ),
+                ),
+                SizedBox(height: 10,),
+              Row(
+                children: [
+                  SizedBox(width: 10,),
+                  Expanded(
+                    child: ElevatedButton(onPressed: () {}, child: Text("All")),
+                  ),
+                  SizedBox(width: 10,),
+                  Expanded(
+                    child: ElevatedButton(onPressed: (){}, child: Text("Favorites")),
+                  ),
+                  SizedBox(width: 10,),
+                  Expanded(child: ElevatedButton(onPressed: (){}, child: Text("Group")),
+                  ),
+                ],
+              ),
+              SizedBox(height: 10,),
+              
+              
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
